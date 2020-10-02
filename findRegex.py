b29114e3011e9d515667713182c4b57e578c4f1a
jdwinfodesign <jdwinfodesign@gmail.com>
2020-10-02 16:53:38 -0400

    finding string

--- findRegex.py
+++ findRegex.py
@@ -10,11 +10,21 @@ import re
 
 file = open('C:\\Users\\jdwin\\Documents\\jdwinfodesign\\doctype-python\\src-use\\c_about_defining_payload.dita','r')
 file_contents = file.read()
+
+# TODO Find string matching regex
+findDoctype = re.compile(r'<!DOCTYPE concept.*>$', re.DOTALL)
+print(findDoctype.search(file_contents))
 print(file_contents)
 file.close()
-# TODO Find string matching regex
 
 # TODO Replace regex with new string
 
 # TODO Save file with new string
 
+foo = re.compile(r'<!DOCTYPE>?')
+f = foo.findall(r'   <!DOCTYPE          >')
+f
+
+
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE concept PUBLIC "-//OASIS//DTD DITA Composite//EN" "ditabase.dtd"
