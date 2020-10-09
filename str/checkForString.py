7b81c1ce3aae20df8302c9f295fe8a0ae87c12a4
jdwinfodesign <jdwinfodesign@gmail.com>
2020-10-09 11:55:25 -0400

    regex working for test-20(1,2).dita

--- str/checkForString.py
+++ str/checkForString.py
@@ -6,15 +6,15 @@ import re
 # Open file
 #fin = open(r'C:\Users\jdwin\Documents\jdwinfodesign\doctype-python\str\src\test-10.dita','r')
 #content = fin.read()
-string = 'DOCTYPE'
+string = '''<!DOCTYPE concept PUBLIC "-//OASIS//DTD DITA Concept//EN"
+"http://docs.oasis-open.org/dita/v1.1/OS/dtd/concept.dtd">'''
 
 def check():
-        with open(r'src\test-10.dita','r') as f:
+        with open(r'src\test-202.dita','r') as f:
                 if string in f.read():
                         print("true")
                 else:
                         print('false')
-        
 
 ##def check():
 ##    datafile = open(r'src\test-10.dita')
