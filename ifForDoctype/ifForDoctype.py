b5f8d9786c80c1b15a471e4b89ee1da3d995e8a3
jdwinfodesign <jdwinfodesign@gmail.com>
2020-10-17 11:43:40 -0400

    Choose and confirm inDir and outDir. Starting doctype if statement.

--- ifForDoctype/ifForDoctype.py
+++ ifForDoctype/ifForDoctype.py
@@ -9,35 +9,44 @@ import xml.etree.ElementTree as et
 ##IT IS INTENDED FOR DITA FILES BUT SHOULD
 ##WORK FOR ANY XML FILES 
 
-# Step 1: CHOOSE DIRECTORY CONTAINING THE FILES TO FIX
-# C:\Users\jdwin\Documents\jdwinfodesign\doctype-python\src
-def chooseDir(rootDir):
-    print('You want to search: ' + rootDir + '?')
+# Step 1: CONFIRM DIRECTORIES CHOSEN
+def verifyDirs(inDir):
+    print('You want to modify the files in: ' + inDir + '?')
     print('Type \'y\' and ENTER to confirm, any other key to abort.')
-    confirm = input()
-    if confirm == 'y':
-        print('ok')
-        walk(rootDir)
+    confirmInDir = input()
+    if confirmInDir == 'y':
+        print(r'Enter the absolute path to direct the output. For example:')
+        print(r'C:\Users\jdwin\Documents\XMLfiles\out')
+        outDir = input()
+        print('You want to direct the output to: ' + outDir + '?')
+        print('Type \'y\' and ENTER to confirm, any other key to abort.')
+        confirmOutDir = input()
+        if confirmOutDir == 'y':
+            print('OK')
+            walk(inDir, outDir)
+        else:
+            print('Goodbye')
+        
     else:
-            print('goodbye')
+            print('Goodbye')
 
 # Step 2: WALK THE CHOSEN DIRECTORY AND ALL SUBDIRECTORIES
 
-def walk(rootDir, outdir):
-    for subdir, dirs, files in os.walk(rootDir):
+def walk(inDir, outDir):
+    for subdir, dirs, files in os.walk(inDir):
         for filename in files:
             filepath = subdir + os.sep + filename
 
             #if filepath.endswith(".jpg") or filepath.endswith(".png"):
             print ('filepath: ' + filepath)
             print ('filename: ' + filename)
-            fixDoctype(rootDir, outdir, filename, filepath)
+            fixDoctype(inDir, outDir, filename, filepath)
 
 # Step 3: COPY EACH FILE IN THE DIRECTORY, REPLACING DOCTYPE
 
-def fixDoctype(rootDir, outdir, filename, filepath):
-##    print('rootDir: ' + rootDir)
-##    print(' outdir: ' + outdir)
+def fixDoctype(inDir, outDir, filename, filepath):
+##    print('inDir: ' + inDir)
+##    print(' outDir: ' + outDir)
 
     parser = etree.XMLParser(strip_cdata=False)
     tree = etree.parse(filepath, parser)
@@ -45,21 +54,20 @@ def fixDoctype(rootDir, outdir, filename, filepath):
     root = tree.getroot()
 ##
 ### TODO replace doctype with variable
-    tree.write(outdir + '\\' + filename, xml_declaration=True, encoding='UTF-8', doctype='''<!DOCTYPE concept PUBLIC "urn:pubid:jdwinfodesign.com:doctypes:dita:dtd:concept" "concept.dtd">''')
+    tree.write(outDir + '\\' + filename, xml_declaration=True, encoding='UTF-8', doctype='''<!DOCTYPE concept PUBLIC "urn:pubid:jdwinfodesign.com:doctypes:dita:dtd:concept" "concept.dtd">''')
 # TODO replace doctype with variable
-    print(' rootDir: ' + rootDir)
-    print('  outdir: ' + outdir)
+    print(' inDir: ' + inDir)
+    print('  outDir: ' + outDir)
     print('filename: ' + filename)
 
-#---------------------------------------------------------------------
+#--------------------------------------------------------------------------------
+# Step 0: Prompt user to choose directory containing
+#         files to be modified (inDir) and
+#         location for output (outDir)
 
-print(r'Enter the absolute path to search. For example:')
-print(r'C:\Users\jdwin\Documents\jdwinfodesign\doctype-python\walkTest')
+print(r'Enter the absolute path to the directory containing files to modify. For example:')
+print(r'C:\Users\jdwin\Documents\XMLfiles\src')
 
-rootDir = input()
-##chooseDir(rootDir)
+inDir = input()
 
-print('Enter the file path to the output directory')
-
-outdir = input()
-walk(rootDir, outdir)
+verifyDirs(inDir)
